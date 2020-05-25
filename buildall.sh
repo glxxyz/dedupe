5cd541fdcb7ad19da0b2b632e242f4af773810f0
Alan Davies <alan@galax.xyz>
2020-05-25 00:36:09 -0400

    Fixed cross platform build

--- buildall.sh
+++ buildall.sh
@@ -1,11 +1,3 @@
-GOOS=darwin
-GOARCH=amd64
-go build -o bin/macos-amd64/dedupe *.go
-
-GOOS=linux
-GOARCH=amd64
-go build -o bin/linux-amd64/dedupe *.go
-
-GOOS=windows
-GOARCH=amd64
-go build -o bin/windows-amd64/dedupe.exe *.go
+env GOOS=darwin GOARCH=amd64 go build -o bin/macos-amd64/dedupe *.go
+env GOOS=linux GOARCH=amd64 go build -o bin/linux-amd64/dedupe *.go
+env GOOS=windows GOARCH=amd64 go build -o bin/windows-amd64/dedupe.exe *.go
